523f7ceba7e033404ff3f0c4f453b649b43e5502
mezan <amasi.jewelery@gmail.com>
2026-07-31 19:12:50 +0300

    Merge pull request #149 from AMASI-SA/fix/review-customer-history-loading
    fix(fulfillment): speed up customer history and show prior orders

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import "@/reviewMezanImageEnhancer";
-import "@/reviewCustomerHistoryEnhancer";
+import "@/reviewCustomerHistoryFast";
 import App from "@/App";
 import PublicLegalApp, { isPublicLegalPath } from "@/PublicLegalApp";
 
