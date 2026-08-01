a338f7d9054cc64b45ce22c28154a74eca072375
mezan <amasi.jewelery@gmail.com>
2026-08-01 20:02:09 +0300

    test(navigation): cover focused marketing provider contract

---
+++ frontend/src/pages/MarketingPlatformWorkspace.test.jsx
@@ -0,0 +1,28 @@
+import {
+    MARKETING_PLATFORM_PROVIDERS,
+    isMarketingPlatformProvider,
+} from "./MarketingPlatformWorkspace";
+
+test("focused marketing workspace accepts only advertising providers", () => {
+    expect(MARKETING_PLATFORM_PROVIDERS).toEqual([
+        "snapchat_ads",
+        "tiktok_ads",
+        "meta_ads",
+        "google_ads",
+    ]);
+
+    MARKETING_PLATFORM_PROVIDERS.forEach((provider) => {
+        expect(isMarketingPlatformProvider(provider)).toBe(true);
+    });
+
+    [
+        "google_analytics_4",
+        "google_merchant_center",
+        "salla",
+        "qoyod",
+        "",
+        null,
+    ].forEach((provider) => {
+        expect(isMarketingPlatformProvider(provider)).toBe(false);
+    });
+});
