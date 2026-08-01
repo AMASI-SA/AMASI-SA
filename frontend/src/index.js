1a7c5e135cdc23641e6ec0ff0e197604efe1d20d
mezan <amasi.jewelery@gmail.com>
2026-08-01 23:06:31 +0300

    feat(fulfillment): enable automatic next-order review advance

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import "@/reviewSpecReplacementEnhancer";
 import "@/reviewCompactActionLabels";
 import "@/reviewInternalPreparationRouteEnhancer";
 import "@/reviewProductEditMode";
+import "@/reviewAutoAdvance";
 import App from "@/App";
 import PublicLegalApp, { isPublicLegalPath } from "@/PublicLegalApp";
 import { applyPublicLegalNoIndex } from "@/publicLegalNoIndex";
