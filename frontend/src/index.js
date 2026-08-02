bbde161d8943a47092c97ae32da6a6f34065bad2
mezan <amasi.jewelery@gmail.com>
2026-08-02 19:05:47 +0300

    feat(preview): enable fulfillment test-data controls

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import "@/reviewAutoAdvance";
 import "@/reviewManualNavigation";
 import "@/reviewCustomerWaiting";
 import "@/reviewPreparationFileRegistryEnhancer";
+import "@/reviewPreviewFulfillmentSeedEnhancer";
 import App from "@/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
