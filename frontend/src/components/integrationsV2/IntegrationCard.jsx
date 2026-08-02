9e1882839528a72f9e6b05a86b42b3324ae8684e
mezan <amasi.jewelery@gmail.com>
2026-08-02 23:03:25 +0300

    fix(prod-apps): restore stable JSX entrypoint

--- frontend/src/components/integrationsV2/IntegrationCard.jsx
+++ frontend/src/components/integrationsV2/IntegrationCard.jsx
@@ -1,14 +1,1 @@
-import AdvertisingAccountCard from "./AdvertisingAccountCard";
-import IntegrationCardV2 from "./IntegrationCardV2";
-
-function isAdvertisingAccountsWorkspace() {
-    if (typeof window === "undefined") return false;
-    return new URLSearchParams(window.location.search).get("workspace") === "accounts";
-}
-
-export default function IntegrationCard({ compactAdvertisingAccounts = false, ...props }) {
-    if (compactAdvertisingAccounts || isAdvertisingAccountsWorkspace()) {
-        return <AdvertisingAccountCard {...props} />;
-    }
-    return <IntegrationCardV2 {...props} />;
-}
+export { default } from "./IntegrationCardV2";
