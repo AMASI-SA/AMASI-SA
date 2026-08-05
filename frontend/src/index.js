bda97691352f7469fe3cdd1a8dddba449f670186
mezan <amasi.jewelery@gmail.com>
2026-08-05 16:23:06 +0300

    fix(dashboard): install selected date request guard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import "@/marketingSnapchatManualRangeGuard";
 import "@/marketingSnapchatDeliveryStateEnhancer";
 import "@/marketingAdsLiveWorkspaceEnhancer";
 import "@/campaignProfitabilityProductCostLink";
+import "@/dashboardVisibleRangeRequestGuard";
 import "@/dashboardExecutivePlatformSpendInterceptor";
 import "@/reviewedPreparationSelection.css";
 import "@/reviewMezanImageEnhancer";
