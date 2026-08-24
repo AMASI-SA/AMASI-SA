8a6a4ea0a84a4aa3372c517ea32b4c93c6b0230c
mezan <amasi.jewelery@gmail.com>
2026-08-24 17:20:49 +0000

    feat(accounting): use unified P01 settlements workspace

--- frontend/src/pages/accounting/AccountingWorkspace.jsx
+++ frontend/src/pages/accounting/AccountingWorkspace.jsx
@@ -3,13 +3,12 @@ import { useSearchParams } from "react-router-dom";
 import { toast } from "sonner";
 import { useOptionalAuth } from "../../context/AuthContext";
 import { getAccountingAccess, getAccountingModuleStatus } from "../../services/accountingModule";
-import LegacyFinancialProviderAppsWorkspace from "../FinancialProviderAppsLegacyWorkspace";
 import AccountingHome from "./AccountingHome";
 import AccountingPermissionsDialog from "./AccountingPermissionsDialog";
+import AccountingSettlements from "./AccountingSettlements";
 import {
     AccessDenied,
     AccountingHeader,
-    ImplementationNotice,
     LoadingBlock,
 } from "./AccountingShared";
 import { OpeningBalancesBlocked, PartialWorkflowPage } from "./AccountingWorkflowPages";
@@ -71,7 +70,7 @@ export default function AccountingWorkspace() {
             ? <LoadingBlock />
             : <AccountingHome status={status} user={user} accountingPermissions={permissions} />;
     } else if (page.id === "settlements") {
-        content = <div className="space-y-5"><ImplementationNotice page={page} /><LegacyFinancialProviderAppsWorkspace /></div>;
+        content = <AccountingSettlements accountingPermissions={permissions} />;
     } else if (page.id === "opening-balances") {
         content = statusLoading ? <LoadingBlock /> : <OpeningBalancesBlocked status={status} />;
     } else {
